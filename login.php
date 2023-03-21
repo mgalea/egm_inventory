b8f4bf4518a1c2792cbe9015a08b56a7f6190694
Mario Galea <mario.s.galea@gmail.com>
2023-03-21 18:39:50 +0100

    first commit

--- login.php
+++ login.php
@@ -3,58 +3,66 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Login</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/login.css">
 
-        <link rel="icon" href="favicon/favicon.png">
+<head>
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="icon" href="favicon/favicon.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 
-        <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
-    </head>
-    <body>
-        <?php
+<body>
+    <?php
 
-if(!isset($_SESSION['user'])){
-  header("Location: PHP/logout.php");
-}
-else if(isset($_SESSION['user']) && $_SESSION['user']!=""){
-  header("Location: index.php");
-}
+    if (!isset($_SESSION['user'])) {
+        header("Location: PHP/logout.php");
+    } else if (isset($_SESSION['user']) && $_SESSION['user'] != "") {
+        header("Location: index.php");
+    }
+    ?>
 
+    <div class="container-fluid">
+        <img class="pt-5 title" src="images/svg/title.svg" />
 
-
-?>
-        <div class="modal">
-            <form action="PHP/login.php" method="POST" class="title">
-                <h1><br></h1>
-                <img src="images/title.svg"/>
-            </form>
-            <form class="modal-content animate" action="PHP/login.php" method="POST">
-                <div class="imgcontainer">
-                    <img src="images/RS_AI.svg" alt="logo" class="logo">
-                </div>
-                <div class="container">
-                    <label for="uname">
-                        <b>Username</b>
-                    </label>
-                    <input type="text" placeholder="Enter Username" name="username" required>
-                    <label for="psw">
-                        <b>Password</b>
-                    </label>
-                    <input type="password" placeholder="Enter Password" name="password" required>
-                    <button type="submit">Login</button>
-                </div>
-            </form>
+        <div class="imgcontainer">
+            <img class="brand_img" src="images/lslga.png" alt="Lagos State Lotterie and Gaming Authority">
         </div>
-    </body>
-    <script type="text/javascript">
 
-    $( window ).on( "load", function() {
-      <?php if(isset($_GET["error"])){ ?>
-        alert("<?php echo $_GET["error"]; ?>");
-      <?php } ?>
+        <form action="PHP/login.php" method="POST">
+
+            <div class="container">
+                <div class="d-flex justify-content-center">
+                    <div class="login-content">
+                        <label class= "font-display" for="username">
+                            <b>Username</b>
+                        </label>
+                        <input type="text" placeholder="Enter Username" name="username" required>
+                        <label class="pt-2 font-display" for="password">
+                            <b>Password</b>
+                        </label>
+                        <input class="fs-sm-2" type="password" placeholder="Enter Password" name="password" required>
+                        <div class="text-center">
+                        <button class="mt-3 btn-lg btn-secondary font-display text-center" type="submit">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <div class="imgcontainer">
+            <img class="brand_img" src="images/RS_AI.svg" alt="brand image">
+        </div>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $(window).on("load", function() {
+        <?php if (isset($_GET["error"])) { ?>
+            alert("<?php echo $_GET["error"]; ?>");
+        <?php } ?>
     });
+</script>
 
-    </script>
 </html>
